414bc089b9498f40989816f248bc57c16b5fd9c6
Marouanerhalib <MarouaneRhalib@hotmail.com>
2020-12-11 12:00:39 +0100

    commit

---
+++ constants/Colors.ts
@@ -0,0 +1,20 @@
+const tintColorLight = '#008080';
+// const tintColorLight = '#1cbaa4';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
